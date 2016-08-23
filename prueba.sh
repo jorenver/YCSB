e695de0042d64630216a5c42eb6aa81fbe2c88b2
Cristian Pisco <metacris93@gmail.com>
2016-08-22 20:16:10 -0500

    las pruebas de los experimentos 5 y 6 se encuentran en 2 carpetas con sus respectivos nombres

--- prueba.sh
+++
@@ -1,6 +0,0 @@
-file="/experimento"
-if [ ! -d experimento ]; then 
-     mkdir experimento   
-fi
-./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -target 5000 -p operationcount=10000 -p recordcount=1000 > experimento/Cantidad_Registro.txt
-#mv Cantidad_Registro.txt  experimento
